bd1308befb5f259a88d622daf9e9de53820fcd63
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2025-02-02 20:22:16 -0300

    Add event listeners to the forms

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 const libraryBooks = []; // Basic placeholder array to store the books we're adding from the frontend as i'm not handling databases just yet
 
 // Them selectors
+const books = document.querySelectorAll("button.book");
 const bookDialogs = document.querySelectorAll("#dialogs");
+const bookForm = document.querySelector("#book-info"); // The form to add books
 
 // Them dialog selectors
 const dialogCancelBtn = document.querySelectorAll("#cancelBtn");
@@ -21,7 +23,7 @@ function Book(name, author, yearOfIssue, description) {
 };
 
 function addBook() {
-
+    
 };
 
 // Them event listeners
@@ -29,12 +31,31 @@ bookBtns.forEach((btn, i) => {
     btn.addEventListener("click", () => {
         bookDialogs[i].showModal();
     })
-})
+});
 
 dialogCancelBtn.forEach((btn, i) => {
     btn.addEventListener("click", () => {
         bookDialogs[i].close();
     });
-})
+});
 
+bookForm.addEventListener("submit", event => {
+    // Stop the default submission behavior from occuring
+    event.preventDefault();
+    // Create a new FormData constructor, firing the FormData event
+    new FormData(bookForm);
+});
 
+bookForm.addEventListener("formdata", event => {
+    console.log("FormData fired");
+
+    // Get the form data and log it
+    const data = event.formData;
+    data.forEach((value, key) => {
+        console.log({key, value});
+    });
+
+    // Close the dialog 
+    const dialog = document.querySelector(".book-add-dialog");
+    dialog.close();  
+});
